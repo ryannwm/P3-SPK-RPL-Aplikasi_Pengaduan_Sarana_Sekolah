70d3b6f3f6a39191a089c86290c1cc1071f2dd69
Adryan <adryanadzanisyafuan@gmail.com>
2026-01-09 10:15:38 +0700

    feat(migration) : add username field to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $primaryKey = 'user_id';
     protected $fillable = [
         'name',
+        'username',
         'email',
         'role',
         'password',
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->enum('role',['admin', 'siswa']);
